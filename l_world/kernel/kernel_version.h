1bfeb055baa0822f0835c481840d955c769f1dc4
samghwww <50562236+samghwww@users.noreply.github.com>
2019-10-18 18:43:57 +0800

    mem

---
+++ l_world/kernel/kernel_version.h
@@ -0,0 +1,14 @@
+/*******************************************************************************
+Copyright (C) 2019 Sam He(HeGuanglin)
+
+Discription:
+    Kerel version define.
+
+History:
+    Date        Author          Notes
+ 2019/10/18     Sam He          The first version
+
+*******************************************************************************/
+
+#define KERNEL_MAJOR_VERSION            0x00000000
+#define KERNEL_MINOR_VERSION            0x00000000
